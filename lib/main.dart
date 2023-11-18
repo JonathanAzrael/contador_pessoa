74718b643ff4309757a5b4b285c64f70b11a2a74
Jonathan Fernandes <designer.azrael@gmail.com>
2023-11-18 16:23:53 -0300

    return to color

--- lib/main.dart
+++ lib/main.dart
@@ -61,7 +61,7 @@ class _HomePageState extends State<HomePage> {
               style: TextStyle(
                   fontSize: 30,
                   fontWeight: FontWeight.w700,
-                  color: Colors.green),
+                  color: Colors.white),
             ),
             Text(
               count.toString(),
